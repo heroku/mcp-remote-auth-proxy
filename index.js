d6eb63fdc438e39f9f6060e4f22d7cd8b915f5e3
Mars Hall <mars@users.noreply.github.com>
2025-06-20 18:02:49 -0700

    Revert Telemetry (#16)
    * Revert "Destroying the session seems to cause a not found during the flow when deployed. (#15)"
    
    This reverts commit 340c9baa384d906876cf74350e8ca47dc4cb519a.
    
    * Revert "Merge pull request #14 from heroku/tl/cp-mjs-files"
    
    This reverts commit a6656b9e1551af4462c363c2bdf71b735dd2eaaa, reversing
    changes made to 816df23b5bec973678e7a42723d2863633093973.
    
    * Revert "Merge pull request #12 from heroku/sbosio/observability"
    
    This reverts commit 816df23b5bec973678e7a42723d2863633093973, reversing
    changes made to 88c973ca533fc1b65024062e2e65d1b18a13e041.

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-import '@dotenvx/dotenvx/config';
-
-// We need to setup tracing before loading Express, otherwise the Express instrumentation will not be applied
-import { maybeSetupTracing } from './lib/tracer.js';
-maybeSetupTracing();
+import 'dotenv/config';
 
 import server from './lib/server.js';
 
--- my-loader.mjs
+++
@@ -1,8 +0,0 @@
-import { register } from 'node:module'
-import { pathToFileURL } from 'node:url'
-
-register('@opentelemetry/instrumentation/hook.mjs', pathToFileURL('./'), {
-  data: {
-    exclude: [/\/node_modules\/oidc-provider\/lib\/actions\/grants\/index.js/]
-  }
-})
