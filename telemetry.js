5823902648f7308aafe4750ac6b05c993e12731c
Timothy Lowrimore <tlowrimore@heroku.com>
2025-06-27 16:33:46 -0600

    Replaces auto instrumentation with lib specific instrumentation

--- telemetry.js
+++ telemetry.js
@@ -1,15 +1,19 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
+import { NetInstrumentation } from '@opentelemetry/instrumentation-net';
+import { UndiciInstrumentation } from '@opentelemetry/instrumentation-undici';
+import { RedisInstrumentation } from '@opentelemetry/instrumentation-redis-4';
 
 const sdk = new NodeSDK({
   traceExporter: new OTLPTraceExporter(),
   instrumentations: [
-    getNodeAutoInstrumentations({
-      '@opentelemetry/instrumentation-fs': {
-        enabled: false,
-      },
-    })
+    new HttpInstrumentation(),
+    new ExpressInstrumentation(),
+    new NetInstrumentation(),
+    new UndiciInstrumentation(),
+    new RedisInstrumentation(),
   ],
 });
 
