6f3d1354d039b2bb5137dd69e158de1496e28490
Timothy Lowrimore <tlowrimore@heroku.com>
2025-06-27 14:14:58 -0600

    updates bin/release to run telemetry

--- telemetry.js
+++ telemetry.js
@@ -1,6 +1,14 @@
+import { register } from 'node:module';
+import { pathToFileURL } from 'node:url';
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+
+register('@opentelemetry/instrumentation/hook.mjs', pathToFileURL('./'));
+
+console.log('--------------------------------');
+console.log('telemetry.js');
+console.log('--------------------------------');
 
 const sdk = new NodeSDK({
   traceExporter: new OTLPTraceExporter(),
@@ -14,10 +22,3 @@ const sdk = new NodeSDK({
 });
 
 sdk.start();
-
-process.on('SIGTERM', () => {
-  sdk.shutdown()
-    .then(() => console.log('Tracing terminated'))
-    .catch((err) => console.log('Error terminating tracing', err))
-    .finally(() => process.exit(0));
-});
