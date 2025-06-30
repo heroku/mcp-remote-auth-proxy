e7f7f36afa3b8e5a552124ddeb9ecd03481563af
Timothy Lowrimore <154477569+tlowrimore-heroku@users.noreply.github.com>
2025-06-30 08:38:18 -0600

    adds basic otel configuration (#25)
    * adds basic otel configuration
    
    * changes exporter from console to OTLP
    
    * updates bin/release to run telemetry
    
    * Reverts release script
    
    * fixes bad import path in release
    
    * rollback bin/release
    
    * directly importing telemetry in index.js
    
    * Replaces auto instrumentation with lib specific instrumentation
    
    * adds otel sdk shutdown handler
    
    * Removes process.exit call from shutdown handler
    
    * Removes redis instrumentation, as we're using an unsupported lib

---
+++ telemetry.js
@@ -0,0 +1,24 @@
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
+import { NetInstrumentation } from '@opentelemetry/instrumentation-net';
+import { UndiciInstrumentation } from '@opentelemetry/instrumentation-undici';
+
+const sdk = new NodeSDK({
+  traceExporter: new OTLPTraceExporter(),
+  instrumentations: [
+    new HttpInstrumentation(),
+    new ExpressInstrumentation(),
+    new NetInstrumentation(),
+    new UndiciInstrumentation(),
+  ],
+});
+
+sdk.start();
+
+process.on('SIGTERM', () => {
+  sdk.shutdown()
+    .then(() => console.info('Tracing terminated'))
+    .catch((err) => console.error('Error terminating tracing', err))
+});
